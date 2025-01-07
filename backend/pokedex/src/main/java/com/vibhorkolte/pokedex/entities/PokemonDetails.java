0dccb7f96983dc575b84bf39af1950a89131a4df
Vibhor-Kolte <vibhorkolte@gmail.com>
2025-01-08 00:53:14 +0530

    [ft_integrate_with_frontend] - RedisSerializer fails due to lombok-Builder on PokemonDetails

--- backend/pokedex/src/main/java/com/vibhorkolte/pokedex/entities/PokemonDetails.java
+++ backend/pokedex/src/main/java/com/vibhorkolte/pokedex/entities/PokemonDetails.java
@@ -1,16 +1,20 @@
 package com.vibhorkolte.pokedex.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PokemonDetails {
     private String name;
     private int id;
-//    private boolean is_default;
+    //private boolean is_default;
     private String location_area_encounters;
     private Species species;
     private int order;
