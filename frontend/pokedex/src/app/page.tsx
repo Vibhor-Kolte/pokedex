fe9235f121826f28baabb135e05fd7f3d921e0c1
Vibhor-Kolte <vibhorkolte@gmail.com>
2025-01-09 02:30:51 +0530

    [ui/ft_get_pokeapi_data] - render pokemon list

--- frontend/pokedex/src/app/page.tsx
+++ frontend/pokedex/src/app/page.tsx
@@ -1,14 +1,25 @@
-import Header from "../../components/Header";
+"use client";
+
+import PokemonCard from "@/components/PokemonCard";
+import Header from "../components/Header";
+import { useGlobalContext } from "@/context/globalContext";
+
 
 export default function Home() {
+  const { pokemonListDetails, loading } = useGlobalContext();
+
   return (
-    <div>
+    <main>
       <Header/>
 
-      <div>
-        <h1>Pokémon List</h1>
-      </div>
-
-    </div> 
+      <section className="min-h-[91vh]">
+        <div className="px-16 py-8 grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {!loading &&
+            pokemonListDetails.map((pokemon: any, index: number) => {
+              return <PokemonCard key={index} pokemon={pokemon} />;
+            })}
+        </div>
+      </section>
+    </main> 
   );
 }
