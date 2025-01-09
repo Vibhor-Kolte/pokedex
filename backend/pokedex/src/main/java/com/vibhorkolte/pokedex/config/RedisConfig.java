54b636e26c5a778c9a3d0f13a5040c54e7eebf5e
Vibhor-Kolte <47689130+Vibhor-Kolte@users.noreply.github.com>
2025-01-09 18:56:31 +0530

    [deployment] - deploy backend (#8)
    * [deployment] - add prod properties
    
    * [deployment] - fix redis default behaviour
    
    * [deployment] - add railway.json

--- backend/pokedex/src/main/java/com/vibhorkolte/pokedex/config/RedisConfig.java
+++ backend/pokedex/src/main/java/com/vibhorkolte/pokedex/config/RedisConfig.java
@@ -1,8 +1,14 @@
 package com.vibhorkolte.pokedex.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
@@ -20,6 +26,21 @@ public class RedisConfig {
 
         return new ReactiveRedisTemplate<>(factory, serializationContext);
     }
+
+    @Bean
+    @Primary
+    @Profile("prod")
+    public ReactiveRedisConnectionFactory reactiveRedisConnectionFactory(
+            @Value("${spring.redis.host}") String redisHost,
+            @Value("${spring.redis.port}") int redisPort,
+            @Value("${spring.redis.username}") String redisUsername,
+            @Value("${spring.redis.password}") String redisPassword) {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(redisHost);
+        configuration.setPort(redisPort);
+        configuration.setUsername(redisUsername);
+        configuration.setPassword(RedisPassword.of(redisPassword));
+
+        return new LettuceConnectionFactory(configuration);
+    }
 }
-
-
