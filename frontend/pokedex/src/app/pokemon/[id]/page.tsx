1df4e6a978ee509e1afed89a873405b6e1293812
Vibhor-Kolte <47689130+Vibhor-Kolte@users.noreply.github.com>
2025-01-09 21:56:18 +0530

    [deployment] - fix frontend vercel errors
    * [deployment] - add prod properties
    
    * [deployment] - fix redis default behaviour
    
    * [deployment] - add railway.json
    
    * [deployment] - fix some eslint errors
    
    * [deployment] - fix
    
    * [deployment] - fix

--- frontend/pokedex/src/app/pokemon/[id]/page.tsx
+++ frontend/pokedex/src/app/pokemon/[id]/page.tsx
@@ -1,22 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 "use client";
+
 import Header from "@/components/Header";
 import { useGlobalContext } from "@/context/globalContext";
 import { typeColor } from "@/utils/colors";
 import { Ruler, Weight } from "lucide-react";
 import Image from "next/image";
-import React, { useEffect } from "react";
+import React, { useEffect, use} from "react";
 import log from 'loglevel';
 
 log.setLevel('info');
 
 interface Props {
-  params: {
+  params: Promise<{
     id: string;
-  };
+  }>;
 }
-function page({ params }: Props) {
+function Page({ params }: Props) {
   const { fetchPokemonByName, loading, activePokemon } = useGlobalContext();
-  const { id } = params;
+  const { id } = use(params);
 
   const barColor = typeColor[activePokemon?.types[0]?.type.name] || "#f9f1f1";
 
@@ -159,4 +162,4 @@ function page({ params }: Props) {
   );
 }
 
-export default page;
+export default Page;
